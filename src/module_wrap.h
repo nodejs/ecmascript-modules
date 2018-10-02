e3b9557647a5ccb23ae48b454087a50e906ca2ba
Myles Borins <mylesborins@google.com>
2018-10-02 00:41:36 -0400

    esm: remove node specifier resolution algorithm
    Refs: https://github.com/nodejs/modules/pull/180
    
    PR-URL: https://github.com/nodejs/ecmascript-modules/pull/6
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: John-David Dalton <john.david.dalton@gmail.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- src/module_wrap.h
+++ src/module_wrap.h
@@ -12,11 +12,6 @@
 namespace node {
 namespace loader {
 
-enum PackageMainCheck : bool {
-    CheckMain = true,
-    IgnoreMain = false
-};
-
 enum ScriptType : int {
   kScript,
   kModule,
@@ -31,8 +26,7 @@ enum HostDefinedOptions : int {
 
 v8::Maybe<url::URL> Resolve(Environment* env,
                             const std::string& specifier,
-                            const url::URL& base,
-                            PackageMainCheck read_pkg_json = CheckMain);
+                            const url::URL& base);
 
 class ModuleWrap : public BaseObject {
  public:
