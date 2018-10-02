47aac7c1ff0ffd7d35058f1e2d88b886eca39ebc
John-David Dalton <john.david.dalton@gmail.com>
2018-10-02 16:09:22 -0700

    esm: Remove --experimental-vm-modules.
    PR-URL: https://github.com/nodejs/ecmascript-modules/pull/6
    Reviewed-By: Guy Bedford <guybedford@gmail.com>
    Reviewed-By: Myles Borins <mylesborins@google.com>
    Reviewed-By: Michaël Zasso <targos@protonmail.com>

--- src/node_config.cc
+++ src/node_config.cc
@@ -91,9 +91,6 @@ static void Initialize(Local<Object> target,
     READONLY_BOOLEAN_PROPERTY("experimentalModules");
   }
 
-  if (env->options()->experimental_vm_modules)
-    READONLY_BOOLEAN_PROPERTY("experimentalVMModules");
-
   if (env->options()->experimental_worker)
     READONLY_BOOLEAN_PROPERTY("experimentalWorker");
 
