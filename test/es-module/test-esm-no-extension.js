1a5d7fe70f2df0163f6f21e159a8a6de86a823aa
Myles Borins <mylesborins@google.com>
2019-03-14 14:58:22 -0700

    fixup: this is better

--- test/es-module/test-esm-no-extension.js
+++ test/es-module/test-esm-no-extension.js
@@ -7,7 +7,12 @@ const assert = require('assert');
 
 const entry = fixtures.path('/es-modules/noext-esm');
 
-const child = spawn(process.execPath, ['--experimental-modules', '--type=module', entry]);
+const child = spawn(process.execPath, [
+  '--experimental-modules',
+  '--type=module',
+  entry
+]);
+
 let stderr = '';
 child.stderr.setEncoding('utf8');
 child.stderr.on('data', (data) => {
