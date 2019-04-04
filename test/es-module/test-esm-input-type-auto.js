aff36867b77145068f3422b61fa5841c0675b838
Geoffrey Booth <webmaster@geoffreybooth.com>
2019-04-04 00:05:45 -0700

    esm: make --input-type=auto tests use string input

--- test/es-module/test-esm-input-type-auto.js
+++ test/es-module/test-esm-input-type-auto.js
@@ -2,29 +2,35 @@
 const common = require('../common');
 const assert = require('assert');
 const exec = require('child_process').execFile;
+const { readFileSync } = require('fs');
 
 const version = process.version;
 
 expect('esm-with-import-statement.js', version);
 expect('esm-with-export-statement.js', version);
-expect('esm-with-import-expression.js', version);
+// TODO: Enable import expression tests once import() is supported
+// in string input.
+// expect('esm-with-import-expression.js', version);
 expect('esm-with-indented-import-statement.js', version);
 
 expect('cjs-with-require.js', version);
-expect('cjs-with-import-expression.js', version);
+// expect('cjs-with-import-expression.js', version);
 expect('cjs-with-property-named-import.js', version);
 expect('cjs-with-property-named-export.js', version);
 expect('cjs-with-string-containing-import.js', version);
 
 expect('print-version.js', version);
-expect('ambiguous-with-import-expression.js', version);
+// expect('ambiguous-with-import-expression.js', version);
 
 expect('syntax-error-1.js', 'SyntaxError', true);
 expect('syntax-error-2.js', 'SyntaxError', true);
 
 function expect(file, want, wantsError = false) {
   const argv = [
-    require.resolve(`../fixtures/es-modules/input-type-auto-scope/${file}`)
+    '--eval',
+    readFileSync(
+      require.resolve(`../fixtures/es-modules/input-type-auto-scope/${file}`),
+      'utf8')
   ];
   const opts = {
     // TODO: Remove when --experimental-modules is unflagged
