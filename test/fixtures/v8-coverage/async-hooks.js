d6c483ea186d1c27a1df6ca919f6f1ed969d135c
Guy Bedford <guybedford@gmail.com>
2019-10-06 14:25:27 -0400

    fix coverage test

--- test/fixtures/v8-coverage/async-hooks.js
+++ test/fixtures/v8-coverage/async-hooks.js
@@ -1,11 +1,27 @@
 const async_hooks = require('async_hooks');
 const common = require('../../common');
 
+const bootstrapIds = new Set();
 const hook = async_hooks.createHook({
-  init: common.mustNotCall(),
-  before: common.mustNotCall(),
-  after: common.mustNotCall(),
-  destroy: common.mustNotCall()
+  init (asyncId, type, triggerAsyncId, resource, bootstrap) {
+    if (bootstrap) {
+      bootstrapIds.add(asyncId);
+      return;
+    }
+    common.mustNotCall();
+  },
+  before (asyncId) {
+    if (bootstrapIds.has(asyncId)) return;
+    common.mustNotCall();
+  },
+  after (asyncId) {
+    if (bootstrapIds.has(asyncId)) return;
+    common.mustNotCall();
+  },
+  destroy (asyncId) {
+    if (bootstrapIds.has(asyncId)) return;
+    common.mustNotCall();
+  }
 });
 
 hook.enable();
